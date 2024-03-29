59add4a108062879a204b2f93346fb87282c5282
Sheng-Lin Yang <141781844+newtyoung0713@users.noreply.github.com>
2024-03-28 22:06:26 -0400

    Add files via upload
    For assignment-4

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /********************************************************************************
-*  WEB322 – Assignment 03
+*  WEB322 – Assignment 04
 * 
 *  I declare that this assignment is my own work in accordance with Seneca's
 *  Academic Integrity Policy:
@@ -8,10 +8,10 @@
 * 
 *  Name: Sheng-Lin Yang Student ID: 160443222 Date: Mar 19th, 2024
 *
-*  https://dull-lime-betta-hose.cyclic.app/
-*
+*  Published URL: ___________________________________________________________
 *
 ********************************************************************************/
+
 
 const express = require('express'); // "require" the Express module
 const path = require('path');
@@ -20,29 +20,51 @@ const legoData = require('./modules/legoSets');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
 legoData.initialize().then(() => {});
 
 // Add your routes here
 // e.g. app.get() { ... }
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/home.html'));
+  res.render('home');
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/about.html'));
+  res.render('about', {page: '/about'});
 });
 
 app.get('/lego/sets', (req, res) => {
+  const themes = req.query.theme;
   legoData.getAllSets()
-          .then(sets => res.json(sets))
-          .catch(error => res.status(404).send({ error: 'Internal Server Error', message: error.message }));
+          .then(sets => {
+            if (themes) {
+              sets = sets.filter(set => set.theme === themes);
+              if (!sets.length) {
+                res.status(404).render("404", { message: `No sets found for theme: ${themes}` });
+                return;
+              }
+            }
+            res.render("sets", {sets: sets, theme: themes});
+          })
+          .catch(error => {
+            res.status(404).send({ error: 'Internal Server Error', message: error.message });
+            res.status(500).render("404", { message: "404." });
+          });
 });
 
 app.get('/lego/sets/:set_num', (req, res) => {
   const numOfLego = req.params.set_num;
   legoData.getSetByNum(numOfLego)
-          .then(set => res.json(set))
-          .catch(error => res.status(404).send({ error: 'Data could NOT find', message: error.message }));
+          .then(set => {
+            if (set) {
+              res.render("setDetail", {set: set})
+            } else {
+              res.status(404).render('404', { message: `Unable to find requested sets.` });
+            }
+          })
+          .catch(error => res.status(404).render('404', { message: `Unable to find requested sets.` }));
 });
 
 // This use() will add an error handler function to
@@ -59,7 +81,7 @@ app.use(function (err, req, res, next) {
 // This means we can use it as a sort of 'catch all' when no route match is found.
 // We use this function to handle 404 requests to pages that are not found.
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
+  res.status(404).render("404", {message: "I'm sorry, we're unable to find what you're looking for"});
 });
 
 // *** DO NOT MODIFY THE LINES BELOW ***
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [`./views/*.html`],
+  mode: 'jit',
+  content: [`./views/**/*.ejs`],
   theme: {
     extend: {},
   },
